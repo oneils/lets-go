674d0fc3ea50a71b8e3987f43e9c7e3a4cdea4da
oneils <lex.nox@gmail.com>
2022-09-01 20:47:53 +0200

    Chapter 3: added leveled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	// create 2 separate loggers for INFO and ERROR messages.
+	// other useful flags:
+	// log.Llongfile - print the full file path, intead of the name only
+	// log.LUTC - print the time in the UTC instead of local
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -16,8 +30,13 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("snippet/create", snippetCreate)
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog, Handler: mux,
+	}
 
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+
+	errorLog.Fatal(err)
 }
