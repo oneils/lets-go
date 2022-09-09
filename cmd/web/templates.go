43b861863d22b947a8eeea9936cbb5d3a9b39f7b
oneils <lex.nox@gmail.com>
2022-09-09 14:17:36 +0200

    Chapter 5: added current year as a common dynamic data

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -9,8 +9,9 @@ import (
 
 // templateData is a the holding structure for any dynamic data that we want to pass to our HTML templates.
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
