59d1a5c8d940628dbb51af78a2afd7e1143ea4b2
oneils <lex.nox@gmail.com>
2022-09-09 13:59:07 +0200

    Chapter 5: Templates caching    - added buffering for the partual templates    - added runtime error handling to display errorrs correctly

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -27,13 +27,21 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-		files := []string{
-			"./ui/html/base.tmpl",
-			"./ui/html/partials/nav.tmpl",
-			page,
+
+		// Parse the base template file into a template set.
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
 		}
 
-		ts, err := template.ParseFiles(files...)
+		// Call ParseGlob() *on this template set* to add any partials.
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseFiles() *on this template set* to add the  page template.
+		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
