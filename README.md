71790e1c00e0f54d190303235d925e2d66d113b8
oneils <lex.nox@gmail.com>
2022-09-12 00:15:39 +0200

    Chapter 10: Security improvements    - used  the TTL certificate    - added connection timeout config

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # lets-go
 Example and exercises from the the Book Let's Go
+
+## Generate self-signed TLS
+
+Create `tls` folder in the project's root:
+
+```bash
+mkdir tls && cd tls
+```
+
+Generate private key and TLS certificate:
+
+```bash
+go run /usr/local/go/src/crypto/tls/generate_cert.go --rsa-bits=2048 --host=localhost 
+```
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"github.com/alexedwards/scs/mysqlstore"
@@ -57,6 +58,10 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	// Make sure that the Secure attribute is set on our session cookies.
+	// Setting this means that the cookie will only be sent by a user's web
+	// browser when a HTTPS connection is being used (and won't be sent over an unsecure HTTP connection).
+	sessionManager.Cookie.Secure = true
 
 	app := application{
 		errorLog:      errorLog,
@@ -67,13 +72,25 @@ func main() {
 		sessionManger: sessionManager,
 	}
 
+	// Initialize a tls.Config struct to hold the non-default TLS settings we
+	//want the server to use. In this case the only thing that we're changing
+	//is the curve preferences value, so that only elliptic curves with
+	// assembly implementations are used.
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog, Handler: app.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 
 	errorLog.Fatal(err)
 }
