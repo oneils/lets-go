4b3650f038f54a7f4e23e1b5d0d2ca8318258aad
oneils <lex.nox@gmail.com>
2022-09-14 20:06:49 +0200

    Chapter 14: moved commong testing logic to the testutils

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"github.com/oneils/lets-go/internal/assert"
 	"io"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -39,32 +38,13 @@ func TestPingUnit(t *testing.T) {
 // example of end-to-end test
 func TestPing(t *testing.T) {
 	// create app with logger, becase logRequest and recoverPanic use loggers.
-	app := &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
+	app := newTestApplication(t)
 
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	// The network address that the test server is listening on is contained in
-	// the ts.URL field. We can  use this along with the ts.Client().Get() method
-	// to make a GET /ping request against the test server. This returns a
-	// http.Response struct containing the response.
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
