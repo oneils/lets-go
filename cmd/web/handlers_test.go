50e1038fbe04ba8af3093eeb62d30a9a26725139
oneils <lex.nox@gmail.com>
2022-09-14 19:55:49 +0200

    Chapter 14: added end-to-end test for ping route

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"github.com/oneils/lets-go/internal/assert"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestPing(t *testing.T) {
+// example of unit testing of handlers
+func TestPingUnit(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	r, err := http.NewRequest(http.MethodGet, "/", nil)
@@ -20,6 +22,39 @@ func TestPing(t *testing.T) {
 	ping(rr, r)
 
 	rs := rr.Result()
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+}
+
+// example of end-to-end test
+func TestPing(t *testing.T) {
+	// create app with logger, becase logRequest and recoverPanic use loggers.
+	app := &application{
+		errorLog: log.New(io.Discard, "", 0),
+		infoLog:  log.New(io.Discard, "", 0),
+	}
+
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+
+	// The network address that the test server is listening on is contained in
+	// the ts.URL field. We can  use this along with the ts.Client().Get() method
+	// to make a GET /ping request against the test server. This returns a
+	// http.Response struct containing the response.
+	rs, err := ts.Client().Get(ts.URL + "/ping")
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	assert.Equal(t, rs.StatusCode, http.StatusOK)
 
